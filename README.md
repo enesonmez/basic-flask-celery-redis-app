a0c2facb8490aae2338e10aac4b0d9608ee22a3b
Enes Sonmez <enes@analyticahouse.com>
2021-10-19 12:06:11 +0300

    fixed readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # basic-flask-celery-redis-app
+
+## Quick start
+to start Flask sever:
+```bash
+$ flask run
+```
+to start Celery asynchronous task queue:
+```bash
+$ celery -A tasks worker --loglevel=info
+```
+If the above doesn't work
+```bash
+$ celery -A tasks worker -l info -P eventlet
+```
+
+## Content
+
+### Pending add example
+#### Start new task
+
+* localhost:5000/pending/start_task -> if you request this route, new task _starts_ and your unique **task_id** returns
+
+#### Check result of task out
+
+* localhost:5000/pending/task_result/<task_id> -> if you request this route with your unique **task_id** it returns _result_ of your task
+
+### Other
+#### Check status of task out
+
+* localhost:5000/task_status/<task_id> -> if you request this route with your unique **task_id** it returns _status_ of your task
--- app.py
+++ app.py
@@ -25,7 +25,7 @@ celeryApp = Celery('celer_worker', broker=CELERY_BROKER_URL, backend=CELERY_RESU
 def main():
     return "Celery - Redis"
 
-@app.route('/start_task')
+@app.route('/pending/start_task')
 def start_task():
     app.logger.info("Call method")
     r = celeryApp.send_task('tasks.wait_add', kwargs={'x':2,'y':2})
@@ -33,14 +33,14 @@ def start_task():
     return r.id
 
 
+@app.route('/pending/task_result/<task_id>')
+def task_result(task_id):
+    result = celeryApp.AsyncResult(task_id).result
+    return "Result of the Task " + str(result)
+
+
 @app.route('/task_status/<task_id>')
 def task_status(task_id):
     status = celeryApp.AsyncResult(task_id, app=celeryApp)
     print("Called status method")
     return "Status of the Task " + str(status.state)
-
-
-@app.route('/task_result/<task_id>')
-def task_result(task_id):
-    result = celeryApp.AsyncResult(task_id).result
-    return "Result of the Task " + str(result)
